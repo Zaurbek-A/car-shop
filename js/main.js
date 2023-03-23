5b8e309172bda1f1c570550fbe512b60bdbefcd7
Zaurbek-A <arinoff.z@gmail.com>
2023-03-23 22:14:02 +0600

    520

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
+})
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function(Item){
+    item.classList.remove('tabs__btn-item--active');
+  })
+
+
+  tabTarget.classList.add('tabs__btn-item--active');
+}
+
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
