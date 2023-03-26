fb23579cb5cada1033e1ed68f380f433d27746c3
Zaurbek-A <arinoff.z@gmail.com>
2023-03-26 15:03:08 +0600

    30

--- js/main.js
+++ js/main.js
@@ -10,13 +10,26 @@ function open(evt) {
   const tabTarget = evt.currentTarget;
   const button = tabTarget.dataset.button;
 
-  tabItem.forEach(function(Item){
+  tabItem.forEach(function(item){
     item.classList.remove('tabs__btn-item--active');
-  })
-
+  });
 
   tabTarget.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach(function(item){
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () =>{
+  menu.classList.toggle('menu__list-active');
+});
+
 
 
 const swiper = new Swiper(".swiper", {
